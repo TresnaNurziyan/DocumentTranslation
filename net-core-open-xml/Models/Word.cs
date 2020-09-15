208aa068497d5c7f73b938ad236df7e2763d720f
Tresna <tresna.nurziyan@gmail.com>
2020-09-15 10:49:23 +0700

    First Commit

---
+++ net-core-open-xml/Models/Word.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_open_xml.Models
+{
+    public class Word
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PlaceofBirth { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string JobTitle { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int? PostalCode { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Nationality { get; set; }
+        public string ProfessionalSummary { get; set; }
+    }
+}
